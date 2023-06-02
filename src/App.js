2359ae053b3293bc0cdf8e590b544c5c3caadef4
patilshubham2001 <shubhamypatil9545@gmail.com>
2023-06-02 23:10:37 +0530

    create Header Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
+      // JSX Code written below
       <div>
-        <div className='header'>
-          Phone Directory
-        </div>
+        <Header/>
         <button> Add </button>
         <div>
           <span>Name</span><br />
@@ -18,4 +18,12 @@ class App extends Component {
   }
 }
 
-export default App;
+ export default App;
+
+// create JSX Element using JavaScript CreateElement Methode
+
+      // React.createElement("div",{id:"inputFieadl"},
+      //   React.createElement("label",{htmlFor: "name"},"First Name: "),
+      //   React.createElement("input",{id:"name", type: "text", placeholder:"enter your first name"}),
+      //   React.createElement("button",{type:"submit",id:"btn-submit", className:"btn"},"Submit")
+      //   )
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+// we create function component for code reUsabality
+const Header = function(){
+    return(
+        <div className='header'>
+            Phone Directory
+        </div>
+    );
+}
+
+export default Header;
