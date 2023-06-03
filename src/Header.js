5f9832f049778108a4bb0cedb1557035adae37a1
patilshubham2001 <shubhamypatil9545@gmail.com>
2023-06-03 20:07:54 +0530

    Styled Header Component

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,17 @@
 import React from 'react';
 
 // we create function component for code reUsabality
-const Header = function(){
-    return(
-        <div className='header'>
+const Header = function () {
+
+    const headerStyle = {
+        textAlign: 'center',
+        padding: 20,
+        background: '#000',
+        color: '#fff',
+        textTransform: 'uppercase'
+    };
+    return (
+        <div style={headerStyle}>
             Phone Directory
         </div>
     );
