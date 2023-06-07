5b0f5a44fa86a1d8776e45438722cd906cae9131
patilshubham2001 <shubhamypatil9545@gmail.com>
2023-06-07 12:21:26 +0530

    Added state in 'AddSubscriber' and 'App' component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import './App.css';
+import './common/common.css'
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      subscriberListToShow: []
+    }
+  }
+
   render() {
 
-    let subscriber = [
-      {
-        id: 1,
-        name: "Shubham",
-        phone: "9032135549"
-      },
-      {
-        id: 2,
-        name: "Ashutosh",
-        phone: "9545623007"
-      }
-    ];
+    // let subscriber = [
+    //   {
+    //     id: 1,
+    //     name: "Shubham",
+    //     phone: "9032135549"
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "Ashutosh",
+    //     phone: "9545623007"
+    //   }
+    // ];
     return (
       // JSX Code written below
-      <div className='component-body-container'>
+      <div>
         <Header heading="Phone Directory" />
+        <div className='component-body-container'>
         <button className='custom-btn add-btn'> Add </button>
         <div className='flex-container'>
           <span className='item-heading'>Name</span><br />
@@ -29,7 +38,7 @@ class App extends Component {
         </div>
 
         {
-          subscriber.map(sub => {
+          this.state.subscriberListToShow.map(sub => {
             return <div key={sub.id} className='flex-container'>
               <span className="item-heading element">{sub.name}</span><br />
               <span className="item-heading element">{sub.phone}</span>
@@ -37,6 +46,7 @@ class App extends Component {
             </div>
           })
         }
+        </div>
       </div>
     );
   }
